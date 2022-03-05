dd96a06d8b7fb6c72cecf50e44b0d01d322b3426
Pedro Raposo <pedroraposoneto@gmail.com>
2022-03-05 11:31:00 -0300

    feat: :sparkles: implementing APP in docker
    implementing APP in docker

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,7 +6,7 @@ const app = Express();
 app.use(Express.json());
 
 app.use('/', (request, response) => {
-  return response.json({ mng: 'Hello Wordl' });
+  return response.json({ mng: 'Hello Wordl!' });
 });
 
 export default app;
